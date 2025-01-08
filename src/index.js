30473a2c0f46fc1a53de130f2d5dbf5a3161180d
GLS Pallavi <glspallavi@gmail.com>
2025-01-08 23:28:37 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    
+    <App />
+);
+
+/*
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+      .register('/service-worker.js')
+      .then((registration) => {
+        console.log('Service Worker registered with scope:', registration.scope);
+      })
+      .catch((error) => {
+        console.error('Error registering Service Worker:', error);
+      });
+  }
+  */
